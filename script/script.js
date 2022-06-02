cff78375eca25ae64e7cd5e9d34961ebff8fd584
farouky <pharouky@gmail.com>
2022-06-02 19:24:36 +0100

    New upDate

--- script/script.js
+++ script/script.js
@@ -15,3 +15,25 @@ overlay.addEventListener('click', () => {
 	overlay.style.display = 'none';
 });
 
+const dropDown = document.querySelector('#header>div>nav>ul>li:nth-child(2)');
+const dropDownList = document.querySelector(
+	'#header>div>nav>ul>li:nth-child(2)>ul',
+);
+dropDown.addEventListener('click', () => {
+	if (dropDownList.style.display === 'block') {
+		dropDownList.style.display = 'none';
+	} else {
+		dropDownList.style.display = 'block';
+	}
+});
+const dropDownF = document.querySelector('#header>div>nav>ul>li:nth-child(1)');
+const dropDownFList = document.querySelector(
+	'#header>div>nav>ul>li:nth-child(1)>ul',
+);
+dropDownF.addEventListener('click', () => {
+	if (dropDownFList.style.display === 'block') {
+		dropDownFList.style.display = 'none';
+	} else {
+		dropDownFList.style.display = 'block';
+	}
+});
